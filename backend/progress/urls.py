9d13e7705cef11123d62d4e02b3ec552534a6815
O-G-W-A-L <ogwalja04@gmail.com>
2025-10-20 17:04:08 +0300

    proress is better tracked, progress app started

---
+++ backend/progress/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+# Import viewsets when they're created
+# from .views import ProgressViewSet, EnrollmentViewSet
+
+# router = DefaultRouter()
+# router.register(r'progress', ProgressViewSet)
+# router.register(r'enrollments', EnrollmentViewSet)
+
+urlpatterns = [
+    # API endpoints will be added here
+    # path('', include(router.urls)),
+]
